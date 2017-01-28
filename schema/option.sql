0e66ccfe37b9ae50485e2d971fd622f5001b5553
ravuri-sandy <rvschowdary55576@gmail.com>
2017-01-28 08:09:54 +0530

    Update option.sql

--- schema/option.sql
+++ schema/option.sql
@@ -1,1 +1,4 @@
-create table option (optionid int unsigned NOT NULL auto_increment,pollid int(100) NOT NULL,option varchar(100) not null,primary key (optionid));
+create table option (optionid int unsigned NOT NULL auto_increment,
+                     pollid int(100) NOT NULL,
+                     option varchar(100) not null,
+                     primary key (optionid));
